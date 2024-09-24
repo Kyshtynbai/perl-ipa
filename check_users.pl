12f14689d671ccf549ff9543f1a19f8ed772efe2
Ivan <imartynow@gmail.com>
2024-09-24 20:34:31 +0300

    Added pager update, fixed orgunit add and print

--- check_users.pl
+++ check_users.pl
@@ -8,11 +8,13 @@ use Getopt::Long;
 my $csv_users; # Input file. Format: fname,lname,email,telnumber,login,pager,orgunit
 my $opt_show; # Show users.
 my $opt_create_users; # Create non-existant users
-my $update_pager; # Update pager
+my $pager; # Update pager
+my $group_file;
 
 GetOptions ('check' => \$opt_show,
 	   		'users=s' => \$csv_users,
-			'pager=s' => \$update_pager,
+			'pager=s' => \$pager,
+			'groups=s' => \$group_file,
 			'make' => \$opt_create_users) or die "Error in command arguments\n";
 
 sub check_user {
@@ -35,22 +37,37 @@ sub check_user {
 sub print_user {
 	my $user = shift;
 	$user->{"Pager Number"} = "N/A" if !$user->{"Pager Number"};
-	$user->{"Org unit"} = "N/A" if !$user->{"Org unit"};
+	$user->{"Org. Unit"} = "N/A" if !$user->{"Org. Unit"};
 	$user->{"Telephone Number"} = "N/A" if !$user->{"Telephone Number"};
 	$user->{"Email address"} = "N/A" if !$user->{"Email address"};
-	print $user->{"First name"} . "," . $user->{"Last name"} . "," . $user->{"Email address"} . "," . $user->{"Telephone Number"} . "," . $user->{"User login"} . "," . $user->{"Pager Number"} . "," . $user->{"Org unit"} . "\n";
+	print $user->{"First name"} . "," . $user->{"Last name"} . "," . $user->{"Email address"} . "," . $user->{"Telephone Number"} . "," . $user->{"User login"} . "," . $user->{"Pager Number"} . "," . $user->{"Org. Unit"} . "\n";
 }
 
 sub create_user {
 	my $user_arref = shift;
 	(my $org = $user_arref->[6]) =~ s/\n+|\s+//g; # Removing newlines, which may be caught in original csv
-	my $ipa_responce = `ipa user-add $user_arref->[4] --first="$user_arref->[1]" --last="$user_arref->[0]" --email="$user_arref->[2]" --phone="$user_arref->[3]" --pager="$user_arref->[5] --orgunit="$org" 2>&1`;
+	my $ipa_responce = `ipa user-add $user_arref->[4] --first="$user_arref->[1]" --last="$user_arref->[0]" --email="$user_arref->[2]" --phone="$user_arref->[3]" --pager="$user_arref->[5]" --orgunit="$org" 2>&1`;
 	if ($ipa_responce !~ /ERROR/i) {
 		print "User $user_arref->[4] created!\n";
 	} else {
 		print "Error while creating user $user_arref->[4]: $ipa_responce\n";
 	}
 }
+
+sub update_pager {
+	my $user = shift;
+	my $pager = shift;
+	my $new_pager;
+	if ($user->{"Pager Number"}) {
+		$new_pager = $user->{"Pager Number"};
+	   	$new_pager .= " ";
+		$new_pager .= $pager;
+	} else {
+		$new_pager = $pager;
+	}
+	print $new_pager . "\n";
+	my $ipa_responce = `ipa user-mod $user->{"User login"} --pager="$new_pager" 2>&1`;
+	}
 
 sub main {
 	open (my $fh, "<", $csv_users) or die "Can't open file $csv_users: $!\n";
@@ -59,6 +76,7 @@ sub main {
 		my $user = &check_user($csv_line[4]);
 		if ($user) {
 			&print_user($user);
+			&update_pager($user, $pager) if $pager;
 		} else {
 			$opt_create_users ? &create_user(\@csv_line) : next;
 		}
